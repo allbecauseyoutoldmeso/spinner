4b948336d48362b068454715f3b786d20957b404
kate gleeson <kate.gleeson@thoughtbot.com>
2021-01-30 18:14:22 +0000

    Set rotation using javascript

--- interface.js
+++ interface.js
@@ -4,9 +4,12 @@ $(document).ready(() => {
   const pickCandidate = () =>
     candidates[Math.floor(Math.random() * candidates.length)]
 
+  const setRotation = () => {
+    document.querySelector(':root').style.setProperty('--rotation', '100deg')
+  }
 
   $('#pick-candidate-button').on('click', () => {
+    setRotation()
     $('#needle-container').toggleClass('spinning')
-    $('#candidate').text(pickCandidate())
   })
 })
